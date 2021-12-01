3614062bc01fab93146063315446dedc30ce067b
stexier96 <stexier@et.esiea.fr>
2021-12-01 10:12:57 +0100

    Création d'un Logger

---
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -0,0 +1,9 @@
+package fr.lernejo.logger;
+
+public class LoggerFactory {
+
+    public static Logger getLogger(String name){
+        //Logger log = new Logger();
+        return null;//log;
+    }
+}
